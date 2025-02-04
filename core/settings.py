2f88276b409da91a14b8dabdaee99d8f461e6bdd
Edwin Niwaha <ed107107@gmail.com>
2025-02-04 20:56:20 +0300

    Update Database Connection

--- core/settings.py
+++ core/settings.py
@@ -147,11 +147,21 @@ WSGI_APPLICATION = "core.wsgi.application"
 ############################### ONLINE DATABASE CONFIGURATION ###############################
 
 # Database configuration (PostgreSQL)
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.getenv("DATABASE_URL", None)  # Use DATABASE_URL if provided
+#     )
+# }
+
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", None)  # Use DATABASE_URL if provided
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,  # Keep the connection alive for 10 minutes
+        ssl_require=True    # Ensure a secure (SSL-encrypted) connection
     )
 }
+
 
 ############################### STATIC AND MEDIA FILES CONFIGURATION ###############################
 
