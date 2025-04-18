d9551c82ac07245871891ce384588ea0058d30c7
Edwin Niwaha <ed107107@gmail.com>
2025-04-18 19:31:36 +0300

    Update About us

--- core/settings.py
+++ core/settings.py
@@ -133,16 +133,16 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 ############################### LOCAL DATABASE CONFIGURATION ###############################
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "default_db_name"),
-        "USER": os.environ.get("DB_USER", "default_user"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "default_password"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("DB_NAME", "default_db_name"),
+#         "USER": os.environ.get("DB_USER", "default_user"),
+#         "PASSWORD": os.environ.get("DB_PASSWORD", "default_password"),
+#         "HOST": os.environ.get("DB_HOST", "localhost"),
+#         "PORT": os.environ.get("DB_PORT", "5432"),
+#     }
+# }
 
 
 ############################### ONLINE DATABASE CONFIGURATION ###############################
