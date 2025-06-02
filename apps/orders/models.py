f4f763aba25ba91b5e86e85d0bd35166fea26218
Edwin Niwaha <ed107107@gmail.com>
2025-06-02 17:55:20 +0300

    Update Cart

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -10,7 +10,7 @@ from django.utils.functional import cached_property
 class Cart(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f"Cart of {self.user.username}"
 
