3236f1ce5dbdca395bd2f495a96ceb4776220e90
Edwin Niwaha <ed107107@gmail.com>
2025-06-02 08:52:08 +0300

    Update CartItem

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -42,6 +42,8 @@ class CartItem(models.Model):
         ProductVolume, on_delete=models.CASCADE
     )  # Link to the correct volume
     quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)  # Set once at creation
+    updated_at = models.DateTimeField(auto_now=True) 
 
     def __str__(self):
         return f"{self.product.name} - {self.volume.volume.ml}ML (x{self.quantity})"
