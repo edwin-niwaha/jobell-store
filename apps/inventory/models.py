b91d24fc76be7695631eb472d35341477836d0a0
Edwin Niwaha <ed107107@gmail.com>
2025-07-14 13:52:00 +0300

    Update inventory

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -1,5 +1,8 @@
+import logging
 from django.db import models
 from apps.products.models import Product
+
+logger = logging.getLogger(__name__)
 
 
 class Inventory(models.Model):
@@ -11,27 +14,24 @@ class Inventory(models.Model):
         default=5, verbose_name="Low Stock Threshold"
     )
     is_out_of_stock = models.BooleanField(default=False, verbose_name="Out of Stock")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
 
     def check_stock_alerts(self):
         """Check stock levels and update stock status."""
         self.is_out_of_stock = self.quantity <= 0
-
-        # Trigger low stock alert if quantity is less than or equal to threshold
         if self.quantity <= self.low_stock_threshold and not self.is_out_of_stock:
             self.send_low_stock_alert()
 
     def send_low_stock_alert(self):
         """Send an alert for low stock."""
-        # Implement notification logic here
-        # For example, sending an email or logging the alert
-        print(
-            f"Low stock alert for {self.product.name}. Current stock: {self.quantity}"
-        )
+        print(f"Low stock alert for {self.product.name}. Current stock: {self.quantity}")
 
     def save(self, *args, **kwargs):
-        # Ensure stock alerts are checked before saving
+        logger.info(f"Before save - created_at: {self.created_at}, updated_at: {self.updated_at}")
         self.check_stock_alerts()
         super().save(*args, **kwargs)
+        logger.info(f"After save - created_at: {self.created_at}, updated_at: {self.updated_at}")
 
     def __str__(self):
         return f"{self.product.name} - Stock: {self.quantity}"
